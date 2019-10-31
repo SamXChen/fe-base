7cc04bce268852385555268616c036a97b2301d8
Sam X Chen <739263563@qq.com>
2019-10-31 19:45:18 +0800

    [update] update custom bind function

--- javascript/【实现】function bind/index.js
+++ javascript/【实现】function bind/index.js
@@ -1,21 +1,13 @@
 function customBind(target, ...args) {
 
     if(target === undefined || target === null) {
-        throw new Error('first parameter can not be undefined or null');
-    }
-    const fn = this;
-    
-    let fnIdx = 0;
-    let fnName = `__custom_bind_fn_name__${fnIdx}`;
-    while(target.__proto__[fnName] !== undefined) {
-        fnIdx++;
-        fnName = `__custom_bind_fn_name__${fnIdx}`;   
+        throw new Error('the first parameter can not be undefined or null');
     }
 
-    target.__proto__[fnName] = fn;
+    const fn = this;
 
     return function(...args2) {
-        return target[fnName](...args, ...args2);
+        return fn.call(target, ...args, ...args2);
     }
 }
 
