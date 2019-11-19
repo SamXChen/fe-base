185bff1bfe53133bf47bbf7fbf42d4f8e4088ef1
Sam X Chen <739263563@qq.com>
2019-11-19 13:29:54 +0800

    [update] throttle tests suites

--- javascript/【实现】throttle/test.jest.js
+++ javascript/【实现】throttle/test.jest.js
@@ -19,20 +19,51 @@ describe('throttle function test suites', () => {
             const throttledFn = throttle(consoleFunction, total / 20)
 
             const delay = 1
+
+            const promises = []
             for(let idx = 0; idx < total; idx++) {
-                setTimeout(() => {
-                    throttledFn(idx)
-                }, delay + idx * 10)
+                promises.push(new Promise(resolve => {
+                    setTimeout(() => {
+                        throttledFn(idx)
+                        resolve()
+                    }, delay + idx)
+                }))
             }
             
-            await new Promise(resolve => {
-                setTimeout(resolve, total * delay * 2)
-            })
+            await Promise.all(promises)
         }
 
         await frequentlyCalledFunction()
         
         expect(console.log.mock.calls.length).toBeGreaterThan(0)
         expect(console.log.mock.calls.length).toBeLessThan(total / 20)
+    })
+
+    test('1s throttled function, should log number only once', async () => {
+
+        const total = 1000
+
+        async function frequentlyCalledFunction() {
+
+            const throttledFn = throttle(consoleFunction, total / 20, true)
+
+            const delay = 1
+
+            const promises = []
+            for(let idx = 0; idx < total; idx++) {
+                promises.push(new Promise(resolve => {
+                    setTimeout(() => {
+                        throttledFn(idx)
+                        resolve()
+                    }, delay)
+                }))
+            }
+            
+            await Promise.all(promises)
+        }
+
+        await frequentlyCalledFunction()
+
+        expect(console.log.mock.calls.length).toBe(1)
     })
 })
