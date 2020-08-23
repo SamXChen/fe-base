049f14263e141e9b0fda45d104fa43919c7a4fdf
Sam X Chen <739263563@qq.com>
2020-08-23 23:07:37 +0800

    [update] toy-react

--- javascript/【实现】toy-react/main.js
+++ javascript/【实现】toy-react/main.js
@@ -1,3 +1,9 @@
+/**
+ * 为什么这里要引入一个没有被使用的 createElement 方法，
+ * 因为 babel 的 jsx 插件中，设置了 jsx 解析后的输出入口，方法名是 createElement
+ * 当 babel 将文本内容翻译后，createElement 这个方法的引用就有着落了
+ * 接着，createElement 的模块引用，就会被 webpack 接管处理
+ */
 import { createElement, Component, render } from './src'
 
 class MyComponent extends Component {
